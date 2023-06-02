90e5b53ffbd258098b2ad2926326210bb8511b4d
noraj <alexandre.zanni@europe.com>
2023-06-03 00:11:11 +0200

    add v3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-web-ext build --overwrite-dest -s ./web-extensions/arch-pkgs -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-wiki -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-aur -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-bugs-fs -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-bugs-t -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-forum-a -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-forum-k -a ./dist
-web-ext build --overwrite-dest -s ./web-extensions/arch-man -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-pkgs -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-wiki -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-aur -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-bugs-fs -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-bugs-t -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-forum-a -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-forum-k -a ./dist
+web-ext build --overwrite-dest -s ./web-extensions/v3/arch-man -a ./dist
